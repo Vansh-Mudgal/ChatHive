aea8b5495a0f2fc22d8c28d47440729885eb7f30
Vansh-Mudgal <itsvansh100@gmail.com>
2025-08-16 01:24:04 +0530

    Fragment More UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,21 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
+    //lifecycle
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    // Image Loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    // Circle ImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    // Kotlin & Coroutines
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
 }
